417bbc3c742e9d0701426ba0acca167eb973595f
punica24@njfu.edu.cn <mingyan130824@gmail.com>
2022-11-08 20:20:20 +0800

    delete scripts

--- scripts/run_RTDMaker.sh
+++
@@ -1,6 +0,0 @@
-python ~/scratch/apps/mingyan/Biotools/RTDmaker/RTDmaker.py ShortReads \
---assemblies 02.stringtie/ --SJ-data 01.star.mapping.02/ \
---SJ-reads 2 1 --genome reference/genome/pg_genomic.fa \
---fastq 00.fastp.filtered.fq/ --tpm 0.1  1 \
---fragment-len 0.7 --antisense-len 0.5 --add intronic \
---keep intermediary --ram 8 --outpath 03_pomeRTD --outname pome --prefix pome
--- scripts/run_download_rnaseq_fastq_from_sra.py
+++
@@ -1,7 +0,0 @@
-import glob
-import subprocess
-
-for prj in glob.glob("metadata/*.txt"):
-    cmd = ['download_rnaseq_fastq_from_sra.py','-i',prj,'-nt','4']
-    print(' '.join(cmd))
-    subprocess.run(cmd)
